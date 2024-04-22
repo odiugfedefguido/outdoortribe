1d0bd22d6ef58cba429f9a8e0089aee76824a3d9
Simone Ruggeri <simone.ruggeri3@studio.unibo.it>
2024-04-22 17:46:36 +0200

    Update DB and upload some photos

--- public/homepage.php
+++ public/homepage.php
@@ -26,7 +26,7 @@ include("./../server/functions.php");
   <main>
     <?php
 
-    $current_user_id = 1;//$_SESSION['user_id'];
+    $current_user_id = 1; //$_SESSION['user_id'];
 
     // Query per ottenere i post delle persone che l'utente segue
     $query = "SELECT post.*, user.name, user.surname 
@@ -45,14 +45,62 @@ include("./../server/functions.php");
     if ($result->num_rows > 0) {
       while ($row = $result->fetch_assoc()) {
     ?>
-        
-      <?php
+      <div class="post-container">
+        <div class="username-container">
+          <img class="user-picture" src="./../assets/icons/profile.svg" alt="username-picture">
+          <p class="username"><?php echo $row['name'] . ' ' . $row['surname']; ?></p>
+        </div>
+        <div class="photo-container">
+          <div class="map">
+            <img src="./../assets/icons/map.svg" alt="post-picture">
+          </div>
+        </div>
+        <div class="info-container">
+          <div class="title">
+            <h2><?php echo $row['title']; ?></h2>
+          </div>
+          <div class="location">
+            <img src="./../assets/icons/location.svg" alt="location-icon">
+            <p><?php echo $row['location']; ?></p>
+          </div>
+          <div class="activity">
+            <img src="./../assets/icons/activity.svg" alt="activity-icon">
+            <p><?php echo $row['activity']; ?></p>
+          </div>
+          <div class="details-countainer">
+            <div class="duration">
+              <img src="./../assets/icons/time.svg" alt="duration-activity-icon">
+              <p><?php echo $row['duration']; ?></p>
+            </div>
+            <div class="length">
+              <img src="./../assets/icons/length.svg" alt="length-activity-icon">
+              <p>Km:<?php echo $row['length']; ?></p>
+            </div>
+            <div class="altitude">
+              <img src="./../assets/icons/altitude.svg" alt="altitude-activity-icon">
+              <p><?php echo $row['altitude']; ?></p>
+            </div>
+            <div class="difficulty-<?php echo strtolower($row['difficulty']); ?>">
+              <p><?php echo $row['difficulty']; ?></p>
+            </div>
+          </div>
+          <div class="rating-likes">
+            <div class="rating">
+              <img src="./../assets/icons/star.svg" alt="rating-icon">
+            </div>
+            <div class="likes">
+              <img src="./../assets/icons/like.svg" alt="like-icon">
+            </div>
+          </div>
+        </div>
+      </div>
+    <?php
       }
     } else {
       echo "Nessun post disponibile";
     }
     $conn->close();
-      ?>
+    ?>
     <div class="empty-space"></div>
   </main>
 
--- server/outdoortribeDB.sql
+++ server/outdoortribeDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 20, 2024 at 07:55 PM
+-- Generation Time: Apr 22, 2024 at 06:06 PM
 -- Server version: 8.0.35
 -- PHP Version: 8.2.4
 
@@ -24,47 +24,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `activity`
---
-
-CREATE TABLE `activity` (
-  `typeActivity` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `idMap` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `description` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `difficulty` int NOT NULL,
-  `duration` int NOT NULL,
-  `km` int NOT NULL,
-  `altitudine` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `activity`
---
-
-INSERT INTO `activity` (`typeActivity`, `idMap`, `description`, `difficulty`, `duration`, `km`, `altitudine`) VALUES
-('Cycling', 'map2', 'Explore the countryside on two wheels', 2, 90, 25, 200),
-('Hiking', 'map1', 'Enjoy a scenic hike through the mountains', 3, 120, 10, 500),
-('Running', 'map3', 'Go for a refreshing run along the river', 1, 60, 8, 50);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `follow`
 --
 
 CREATE TABLE `follow` (
+  `follow_id` int NOT NULL,
   `follower_id` int NOT NULL,
   `followed_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `follow`
 --
 
-INSERT INTO `follow` (`follower_id`, `followed_id`) VALUES
-(3, 1),
-(1, 2),
-(2, 3);
+INSERT INTO `follow` (`follow_id`, `follower_id`, `followed_id`) VALUES
+(1, 1, 3),
+(2, 1, 7),
+(3, 1, 9),
+(4, 2, 5),
+(5, 2, 6),
+(6, 2, 7),
+(7, 3, 8),
+(8, 3, 12),
+(9, 4, 10),
+(10, 4, 13),
+(12, 5, 2),
+(11, 5, 4),
+(13, 6, 1),
+(14, 7, 13);
 
 -- --------------------------------------------------------
 
@@ -73,17 +60,37 @@ INSERT INTO `follow` (`follower_id`, `followed_id`) VALUES
 --
 
 CREATE TABLE `photo` (
-  `idFoto` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id` int NOT NULL,
+  `post_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `photo`
 --
 
-INSERT INTO `photo` (`idFoto`) VALUES
-('photo1.jpg'),
-('photo2.jpg'),
-('photo3.jpg');
+INSERT INTO `photo` (`id`, `post_id`, `user_id`, `name`) VALUES
+(1, 10, 1, 'adventure1'),
+(2, 2, 2, 'adventure2'),
+(3, 4, 2, 'adventure3'),
+(4, 11, 2, 'adventure4'),
+(5, 12, 3, 'adventure5'),
+(6, 3, 3, 'adventure6'),
+(7, 14, 1, 'adventure7'),
+(8, NULL, 1, 'man1'),
+(9, NULL, 2, 'woman1'),
+(10, NULL, 3, 'man2'),
+(11, NULL, 4, 'man3'),
+(12, NULL, 5, 'woman2'),
+(13, NULL, 6, 'man4'),
+(14, NULL, 7, 'woman3'),
+(15, NULL, 8, 'man5'),
+(16, NULL, 9, 'woman4'),
+(17, NULL, 10, 'man6'),
+(18, NULL, 11, 'woman5'),
+(19, NULL, 12, 'man7'),
+(20, NULL, 13, 'woman6');
 
 -- --------------------------------------------------------
 
@@ -111,10 +118,14 @@ CREATE TABLE `post` (
 --
 
 INSERT INTO `post` (`id`, `user_id`, `title`, `location`, `activity`, `duration`, `length`, `altitude`, `difficulty`, `rating`, `likes`, `created_at`) VALUES
-(1, 1, 'Escursione sul Monte Bianco', 'Monte Bianco, Italia', 'Escursionismo', '04:00:00', 10, 4000, 'Hard', 5, 25, '2024-04-20 08:40:12'),
-(2, 2, 'Gita in bicicletta al lago', 'Lago di Garda, Italia', 'Ciclismo', '02:00:00', 20, 200, 'Easy', 4, 30, '2024-04-20 08:40:12'),
-(3, 3, 'Trekking nei Pirenei', 'Pirenei, Spagna', 'Trekking', '02:00:00', 15, 2500, 'Medio', 3, 20, '2024-04-20 08:40:12'),
-(4, 2, 'Escursione nella Foresta Nera', 'Foresta Nera, Germania', 'Trekking', '03:00:00', 10, 1000, 'Medium', 4, 20, '2024-04-20 17:42:16');
+(2, 2, 'Gita in bicicletta al lago', 'Lago di Garda, Italia', 'Cycling', '02:00:00', 20, 200, 'Easy', 4, 30, '2024-04-20 08:40:12'),
+(3, 3, 'Trekking nei Pirenei', 'Pirenei, Spagna', 'Trekking', '02:00:00', 15, 2500, 'Medium', 3, 20, '2024-04-20 08:40:12'),
+(4, 2, 'Escursione nella Foresta Nera', 'Foresta Nera, Germania', 'Trekking', '03:00:00', 10, 1000, 'Medium', 4, 20, '2024-04-20 17:42:16'),
+(10, 1, 'Escursione sul lago', 'Lago di Como, Italia', 'Trekking', '02:00:00', 5, 200, 'Easy', 4, 30, '2024-04-22 13:16:47'),
+(11, 2, 'Gita in montagna', 'Monte Bianco, Italia', 'Hiking', '04:00:00', 10, 1500, 'Medium', 4, 25, '2024-04-22 13:16:47'),
+(12, 3, 'Ciclismo costiero', 'Costa Amalfitana, Italia', 'Cycling', '03:00:00', 20, 100, 'Easy', 4, 40, '2024-04-22 13:16:47'),
+(13, 2, 'Escursione nei boschi', 'Foresta Nera, Germania', 'Trekking', '05:00:00', 15, 50, 'Medium', 4, 35, '2024-04-22 13:16:47'),
+(14, 1, 'Tour in bicicletta', 'Toscana, Italia', 'Cycling', '06:00:00', 30, 500, 'Hard', 4, 20, '2024-04-22 13:16:47');
 
 -- --------------------------------------------------------
 
@@ -137,7 +148,17 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `name`, `surname`, `email`, `password`) VALUES
 (1, 'mario', 'rossi', 'mario.rossi@gmail.com', '123'),
 (2, 'anna', 'verdi', 'anna.verdi@gmail.com', '456'),
-(3, 'franco', 'gialli', 'franco.gialli@gmail.com', '789');
+(3, 'franco', 'gialli', 'franco.gialli@gmail.com', '789'),
+(4, 'Marco', 'Rossi', 'marco.rossi@example.com', 'password1'),
+(5, 'Giulia', 'Bianchi', 'giulia.bianchi@example.com', 'password2'),
+(6, 'Luca', 'Ricci', 'luca.ricci@example.com', 'password3'),
+(7, 'Sara', 'Martini', 'sara.martini@example.com', 'password4'),
+(8, 'Matteo', 'Moretti', 'matteo.moretti@example.com', 'password5'),
+(9, 'Alessia', 'Conti', 'alessia.conti@example.com', 'password6'),
+(10, 'Davide', 'Ferrari', 'davide.ferrari@example.com', 'password7'),
+(11, 'Francesca', 'Barbieri', 'francesca.barbieri@example.com', 'password8'),
+(12, 'Giovanni', 'Galli', 'giovanni.galli@example.com', 'password9'),
+(13, 'Chiara', 'Gatti', 'chiara.gatti@example.com', 'password10');
 
 -- --------------------------------------------------------
 
@@ -157,23 +178,20 @@ CREATE TABLE `waypoints` (
 --
 
 --
--- Indexes for table `activity`
---
-ALTER TABLE `activity`
-  ADD PRIMARY KEY (`typeActivity`);
-
---
 -- Indexes for table `follow`
 --
 ALTER TABLE `follow`
-  ADD PRIMARY KEY (`follower_id`),
-  ADD UNIQUE KEY `IDFOLLOW_1` (`followed_id`);
+  ADD PRIMARY KEY (`follow_id`),
+  ADD UNIQUE KEY `follower_id` (`follower_id`,`followed_id`),
+  ADD KEY `followed_id` (`followed_id`);
 
 --
 -- Indexes for table `photo`
 --
 ALTER TABLE `photo`
-  ADD PRIMARY KEY (`idFoto`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `post_id` (`post_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `post`
@@ -200,16 +218,28 @@ ALTER TABLE `waypoints`
 --
 
 --
+-- AUTO_INCREMENT for table `follow`
+--
+ALTER TABLE `follow`
+  MODIFY `follow_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `photo`
+--
+ALTER TABLE `photo`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
 -- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `waypoints`
@@ -220,6 +250,20 @@ ALTER TABLE `waypoints`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `follow`
+--
+ALTER TABLE `follow`
+  ADD CONSTRAINT `follow_ibfk_1` FOREIGN KEY (`follower_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `follow_ibfk_2` FOREIGN KEY (`followed_id`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `photo`
+--
+ALTER TABLE `photo`
+  ADD CONSTRAINT `photo_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`),
+  ADD CONSTRAINT `photo_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 --
 -- Constraints for table `post`
