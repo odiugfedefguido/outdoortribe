95821f47c47fe2973ca8fe2514e32a13d71eb042
Simone Ruggeri <simone.ruggeri3@studio.unibo.it>
2024-04-27 08:58:53 +0200

    Added other sections (data,images,share) to post_details

--- public/post_details.php
+++ public/post_details.php
@@ -15,6 +15,7 @@ include("./../server/functions.php");
   <title>Post Details</title>
   <link rel="stylesheet" href="./../templates/styles/header.css">
   <link rel="stylesheet" href="./../templates/styles/footer.css">
+  <link rel="stylesheet" href="./../templates/styles/components.css">
   <link rel="stylesheet" href="./../templates/styles/post.css">
   <link rel="stylesheet" href="./styles/post_details.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -64,6 +65,58 @@ include("./../server/functions.php");
               <div class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo nihil recusandae nostrum reprehenderit incidunt sint non, voluptatem temporibus. Doloremque, expedita quas tempore voluptate iusto facilis nihil alias qui possimus quo?</div>
             </div>
           </div>
+        </div>
+        
+        <div class="technical-data">
+          <h2>Technical Data</h2>
+          <div class="grid-data">
+            <div class="level">
+              <p class="paragraph-450">Level:</p>
+              <p class="paragraph-400">Lorem </p>
+            </div>
+            <div class="length">
+              <p class="paragraph-450">Length:</p>
+              <p class="paragraph-400">Lorem </p>
+            </div>
+            <div class="ascent">
+              <p class="paragraph-450">Total ascent:</p>
+              <p class="paragraph-400">Lorem </p>
+            </div>
+            <div class="descent">
+              <p class="paragraph-450">Total descent:</p>
+              <p class="paragraph-400">Lorem </p>
+            </div>
+            <div class="max-altitude">
+              <p class="paragraph-450">Max altitude:</p>
+              <p class="paragraph-400">Lorem </p>
+            </div>
+            <div class="min-altitude">
+              <p class="paragraph-450">Min altitude:</p>
+              <p class="paragraph-400">Lorem </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="images">
+          <h2>Images</h2>
+          <div class="image-scroll-container">
+            <div class="image-container">
+              <img src="./../assets/photos/post/adventure1.png" alt="Post Image">
+              <img src="./../assets/photos/post/adventure2.png" alt="Post Image">
+              <img src="./../assets/photos/post/adventure3.png" alt="Post Image">
+              <img src="./../assets/photos/post/adventure4.png" alt="Post Image">
+              <!-- <a href="tutte_le_immagini.html" class="view-all">View All</a> -->
+              <div class="view-all-container">
+                <h2 class="view-all"><a href="tutte_le_immagini.html"></a>View All</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="share">
+          <h2>Have you done this activity?</h2>
+          <p class="paragraph-400">Add it to your activities and share it with your friends!</p>
+          <input class="full-btn" type="button" value="Add and Share">
         </div>
       <?php
       } else {
