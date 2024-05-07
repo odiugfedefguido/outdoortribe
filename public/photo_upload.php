baed4638a2e993c2a18f36750f0623f4125a9129
vasile.dragus <vasile.dragus@studio.unibo.it>
2024-05-07 18:05:55 +0200

    created photo_upload page

---
+++ public/photo_upload.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en-IT">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/svg+xml" href="../assets/icons/favicon.svg">
+  <link rel="stylesheet" href="/outdoortribe/templates/footer/footer.css">
+  <link rel="stylesheet" href="/outdoortribe/templates/header/header.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+  <title>Images Upload</title>
+</head>
+<body>
+  <?php include("./../templates/header/header.html"); ?>
+  <main class="outer-flex-container">
+
+  </main>
+  <?php include("./../templates/footer/footer.html"); ?>
+</body>
+</html>
