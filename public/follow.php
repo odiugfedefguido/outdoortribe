cdaf4750b9e4fdc0c5e9f8bb40a6c1b0ec892220
Fede <federica.guiducci3@studio.unibo.it>
2024-05-08 13:53:02 +0200

    fix some css bugs

--- public/follow.php
+++ public/follow.php
@@ -17,7 +17,7 @@ include("./../admin/functions.php");
   <link rel="stylesheet" href="./../templates/header/header.css">
   <link rel="stylesheet" href="./../templates/footer/footer.css">
  
-  <link rel="stylesheet" href="./styles/follower.css">
+  <link rel="stylesheet" href="./styles/followed.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
@@ -32,52 +32,55 @@ include("./../admin/functions.php");
 $current_user_id = isset($_GET['followed_id']) ? $_GET['followed_id'] : null; // Recupera l'ID dell'utente dall'URL
 
 if($current_user_id !== null) {
-    //query per ottenere i follower
+    //query per ottenere i followed
     $query = "SELECT user.name, user.surname, user.id
           FROM user
-          JOIN follow ON user.id = follow.follower_id
-          WHERE follow.followed_id = ?";
+          JOIN follow ON user.id = follow.followed_id
+          WHERE follow.follower_id = ?";
     $stmt = $conn->prepare($query);
     $stmt->bind_param("i", $current_user_id);
     $stmt->execute();
     $result = $stmt->get_result();
 
-    //stampo il nuemro di follower
-    echo '<h1>Followers</h1>';
-    echo '<p>Number of followers: '.$result->num_rows.'</p>';
+    //stampo il nuemro di followed
+    echo '<h1>Followed</h1>';
+    echo '<p>Number of followed: '.$result->num_rows.'</p>';
 
-    //stampo i follower
+    //stampo i followed
     while($row = $result->fetch_assoc()) {
-        $follower_id = $row['id'];
-        $follower_name = $row['name'];
-        $follower_surname = $row['surname'];
-        //query per ottenere l'immagine del follower
-        $query_image = "SELECT name
-            FROM photo
-            WHERE user_id = ? AND post_id IS NULL";
-
-        $stmt_image = $conn->prepare($query_image);
-        $stmt_image->bind_param("i", $follower_id);
-        $stmt_image->execute();
-        $result_image = $stmt_image->get_result();
-        
-        // Controllo se esiste un'immagine per il follower
-        if ($result_image->num_rows > 0) {
-            $photo_profile_row = $result_image->fetch_assoc();
-            $follower_image = "./../uploads/photos/profile/" . $photo_profile_row['name'];
-        } else {
-            // Se non c'è un'immagine per il follower, utilizzo un'immagine predefinita o mostro un messaggio
-            $follower_image = "default_profile_image.jpg"; // Immagine predefinita
-        }
-
-        //stampo la foto del follower
-        echo '<div class="follower">';
-        echo '<img class="circular-square-img" src="'.$follower_image.'" alt="profile picture">';
-        echo '<a href="otherprofile.php?id='.$follower_id.'" class="profile-link">'.$follower_name.' '.$follower_surname.'</a>';
-                    
-        echo '<a href="profilepage.php?id='.$follower_id.'" class="view-profile-button full-btn">View profile</a>';
-        echo '</div>';
-    }   
+      $followed_id = $row['id'];
+      $followed_name = $row['name'];
+      $followed_surname = $row['surname'];
+      
+      // Query per ottenere l'immagine del followed
+      $query_image = "SELECT name
+          FROM photo
+          WHERE user_id = ? AND post_id IS NULL";
+      
+      // Esegui la query per ottenere l'immagine del followed
+      $stmt_image = $conn->prepare($query_image);
+      $stmt_image->bind_param("i", $followed_id);
+      $stmt_image->execute();
+      $result_image = $stmt_image->get_result();
+  
+      // Controllo se esiste un'immagine per il followed
+      if ($result_image->num_rows > 0) {
+          $photo_profile_row = $result_image->fetch_assoc();
+          $followed_image = "./../uploads/photos/profile/" . $photo_profile_row['name'];
+      } else {
+          // Se non c'è un'immagine per il followed, utilizzo un'immagine predefinita o mostro un messaggio
+          $followed_image = "default_profile_image.jpg"; // Immagine predefinita
+      }
+  
+      //stampo la foto del followed
+      echo '<div class="follower">';
+      echo '<img class="circular-square-img" src="'.$followed_image.'" alt="profile picture">';
+      echo '<a href="otherprofile.php?id='.$followed_id.'" class="profile-link">'.$followed_name.' '.$followed_surname.'</a>';
+                  
+      echo '<a href="profilepage.php?id='.$followed_id.'" class="view-profile-button full-btn">View profile</a>';
+      echo '</div>';
+  }  
+     
 
 } else {
     echo '<p>No user ID provided.</p>';
--- public/followers.php
+++ public/followers.php
@@ -17,7 +17,7 @@ include("./../admin/functions.php");
   <link rel="stylesheet" href="./../templates/header/header.css">
   <link rel="stylesheet" href="./../templates/footer/footer.css">
  
-  <link rel="stylesheet" href="./styles/follower.css">
+  <link rel="stylesheet" href="./styles/followed.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
