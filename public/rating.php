2a1ea4ad1ee500a3b50a947baf479738653bbbec
vasile.dragus <vasile.dragus@studio.unibo.it>
2024-05-08 16:49:33 +0200

    created rating page

---
+++ public/rating.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en-IT">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/svg+xml" href="../assets/icons/favicon.svg">
+  <link rel="stylesheet" href="/outdoortribe/templates/footer/footer.css">
+  <link rel="stylesheet" href="/outdoortribe/templates/header/header.css">
+  <link rel="stylesheet" href="./../templates/components/components.css">
+  <link rel="stylesheet" href="styles/rating.css">
+  <title>Rating</title>
+</head>
+<body>
+  <?php include("./../templates/header/header.html"); ?>
+  <main class="outer-flex-container">
+    <div class="upper-container">
+      <a href="./photo_upload.php" class="back-button">
+        <img class="back-icon" src="/outdoortribe/assets/icons/back-icon.svg" alt="back-icon">
+      </a>
+      <h1>Almost There!</h1>
+    </div>
+  </main>
+  <?php include("./../templates/footer/footer.html"); ?>
+</body>
+</html>
