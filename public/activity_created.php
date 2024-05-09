0ce7b964f40d00b3ea4784e3c26e58b64760b7de
vasile.dragus <vasile.dragus@studio.unibo.it>
2024-05-09 18:06:57 +0200

    created activity_created page with css rules and connected it to rating page

--- public/activity_created.php
+++ public/activity_created.php
@@ -7,37 +7,25 @@
   <link rel="stylesheet" href="/outdoortribe/templates/footer/footer.css">
   <link rel="stylesheet" href="/outdoortribe/templates/header/header.css">
   <link rel="stylesheet" href="./../templates/components/components.css">
-  <link rel="stylesheet" href="styles/rating.css">
+  <link rel="stylesheet" href="./styles/activity_created.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
-  <title>Rating</title>
+  <title>Activity Created</title>
 </head>
 <body>
   <?php include("./../templates/header/header.html"); ?>
   <main class="outer-flex-container">
-    <div class="upper-container">
-      <a href="./photo_upload.php" class="back-button">
-        <img class="back-icon" src="/outdoortribe/assets/icons/back-icon.svg" alt="back-icon">
-      </a>
-      <h1>Almost There!</h1>
+    <h1>Activity Created!</h1>
+    <div class="images-container">
+      <img src="./../assets/icons/fireworks.svg" alt="fireworks-image">
+      <img src="./../assets/icons/mountain.svg" alt="mountain-image">
+    </div>  
+    <div class="message-container">
+      <p><span>OutdoorTribe</span> thanks you for growing the activity database!</p>
+      <p>Thank you for your support</p>
     </div>
-    <div class="rating-wrapper">
-      <h2>Rate the route!</h2>
-      <div class="ratings">
-        <span class="star">&#9733</span>
-        <span class="star">&#9733</span>
-        <span class="star">&#9733</span>
-        <span class="star">&#9733</span>
-        <span class="star">&#9733</span>
-      </div>
-      
-    </div>
-    <div class="buttons-container">
-        <button class="full-btn" onclick="window.location.href = 'activity_created.php';">Create Activity</button>
-      </div>
   </main>
   <?php include("./../templates/footer/footer.html"); ?>
-  <script src="./javascript/rating.js"></script>
 </body>
 </html>
