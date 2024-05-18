85001ec535570f148b05d9b814e8d89672b612eb
vasile.dragus <vasile.dragus@studio.unibo.it>
2024-05-18 11:45:08 +0200

    created find_friends page

---
+++ public/find_friends.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en-IT">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/svg+xml" href="../assets/icons/favicon.svg">
+  <link rel="stylesheet" href="./../templates/header/header.css">
+  <link rel="stylesheet" href="./../templates/footer/footer.css">
+  <link rel="stylesheet" href="styles/find_friends.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+  <title>Find Friends</title>
+</head>
+<body>
+  <?php include("./../templates/header/header.html"); ?>
+  <main class="outer-flex-container">
+    <h1>Find your Friends</h1>
+    <div class="form-container">
+      <form action="" method="get">
+        <label for="friend-name">Name and Surname</label>
+        <div class="search-box">
+          <input type="text" id="friend-name" name="friend-name" placeholder="Search" required>
+          <button type="submit">
+            <img src="../assets/icons/search-alt.svg" alt="Search">
+          </button>
+        </div>
+      </form>
+    </div>
+  </main>
+  <?php include("./../templates/footer/footer.html"); ?>
+</body>
+</html>
