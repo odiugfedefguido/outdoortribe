51d3f12a59b079c4098d610b09c614eac051ccef
Fede <federica.guiducci3@studio.unibo.it>
2024-06-01 10:08:40 +0200

    fix done page

--- public/adventurescreated.php
+++ public/adventurescreated.php
@@ -20,7 +20,7 @@ include("./../admin/functions.php");
   <link rel="stylesheet" href="./../templates/header/header.css">
   <link rel="stylesheet" href="./../templates/footer/footer.css">
   <link rel="stylesheet" href="./../templates/post/post.css">
-  <link rel="stylesheet" href="./../styles/adventure.css">
+  <link rel="stylesheet" href="./styles/createdactivity.css">
   <link rel="icon" type="image/svg+xml" href="./../assets/icons/favicon.svg">
  
   <!-- Collegamento al font Roboto -->
@@ -35,73 +35,76 @@ include("./../admin/functions.php");
   <?php include("./../templates/header/header.html"); ?>
 
   <main>
-  <?php 
+    <?php 
 
-$current_user_id = 2; //$_SESSION['user_id'];
+    $current_user_id = 2; //$_SESSION['user_id'];
 
-//query per ottenere i post dell'utente
-$query = "SELECT post.title, post.location, post.user_id, post.duration, post.length, post.max_altitude, post.difficulty, post.activity, post.likes,
-          (SELECT COUNT(*) FROM likes WHERE post_id = post.id AND user_id = ?) AS user_liked
-      FROM post
-      WHERE post.user_id = ?";
-$stmt = $conn->prepare($query);
-$stmt->bind_param("ii", $current_user_id, $current_user_id);
-$stmt->execute();
-$result = $stmt->get_result();
+    //query per ottenere i post dell'utente
+    $query = "SELECT post.title, post.location, post.user_id, post.duration, post.length, post.max_altitude, post.difficulty, post.activity, post.likes,
+              (SELECT COUNT(*) FROM likes WHERE post_id = post.id AND user_id = ?) AS user_liked
+          FROM post
+          WHERE post.user_id = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("ii", $current_user_id, $current_user_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-//query per ottenere il nome e il cognome dell'utente
-$query_user = "SELECT name, surname
-          FROM user
-          WHERE id = ?";
-$stmt_user = $conn->prepare($query_user);
-$stmt_user->bind_param("i", $current_user_id);
-$stmt_user->execute();
-$result_user = $stmt_user->get_result();
-$user = $result_user->fetch_assoc();
-
-
-//stampo il numero di post creati
-echo '<h1>Posts created</h1>';
-echo '<p>Number of posts created: '.$result->num_rows.'</p>';
-
-//stampo i post creati
-if($result->num_rows > 0) {
-  while($row = $result->fetch_assoc()) {
-    //recuperra l'url dell'immagine del profilo, il rating medio del post e i nomi degli utenti che hanno messo like
-    $profile_photo_url = getProfilePhotoUrl($conn, $row['user_id']);
-    $average_rating = getAverageRating($conn, $row['user_id']);
-    list($full_stars, $half_star) = getStars($average_rating);
-    
-
-    $post_id = $row['user_id'];
-    $username = $user['name'] . ' ' . $user['surname'];
-    $title = $row['title'];
-    $location = $row['location'];
-    $activity = $row['activity'];
-    
-    $duration = $row['duration'];
-    $length = $row['length'];
-    $altitude = $row['max_altitude'];
-    $difficulty = $row['difficulty'];
-    $likes = isset($row['likes']) ? $row['likes'] : 0; // Controlla se il campo 'likes' è impostato nell'array $row
-    $user_liked = $row['user_liked']; // Ottieni il valore di 'user_liked' dall'array $row
-
-    $is_post_details = false;
-    $like_icon_class = $user_liked ? 'like-icon liked' : 'like-icon';
+    //query per ottenere il nome e il cognome dell'utente
+    $query_user = "SELECT name, surname
+              FROM user
+              WHERE id = ?";
+    $stmt_user = $conn->prepare($query_user);
+    $stmt_user->bind_param("i", $current_user_id);
+    $stmt_user->execute();
+    $result_user = $stmt_user->get_result();
+    $user = $result_user->fetch_assoc();
 
 
-    include ('./../templates/post/post.php');
-}
-} else {
-    echo "No posts available";
-}
+    //stampo il numero di post creati
+    echo '<h1>Posts created</h1>';
+    echo '<p>Number of posts created: '.$result->num_rows.'</p>';
 
-$conn->close();
-?>
+    //stampo i post creati
+    if($result->num_rows > 0) {
+      while($row = $result->fetch_assoc()) {
+        //recuperra l'url dell'immagine del profilo, il rating medio del post e i nomi degli utenti che hanno messo like
+        $profile_photo_url = getProfilePhotoUrl($conn, $row['user_id']);
+        $average_rating = getAverageRating($conn, $row['user_id']);
+        list($full_stars, $half_star) = getStars($average_rating);
+        
+
+        $post_id = $row['user_id'];
+        $username = $user['name'] . ' ' . $user['surname'];
+        $title = $row['title'];
+        $location = $row['location'];
+        $activity = $row['activity'];
+        
+        $duration = $row['duration'];
+        $length = $row['length'];
+        $altitude = $row['max_altitude'];
+        $difficulty = $row['difficulty'];
+        $likes = isset($row['likes']) ? $row['likes'] : 0; // Controlla se il campo 'likes' è impostato nell'array $row
+        $user_liked = $row['user_liked']; // Ottieni il valore di 'user_liked' dall'array $row
+
+        $is_post_details = false;
+        $like_icon_class = $user_liked ? 'like-icon liked' : 'like-icon';
 
 
-  </main>
-  <?php include("./../templates/footer/footer.html"); ?>
+        include ('./../templates/post/post.php');
+    }
+    } else {
+        echo "No posts available";
+    }
+
+    $conn->close();
+    ?>
+    <!-- Spazio vuoto per la formattazione -->
+    <div class="empty-space"></div>
+
+    </main>
+    <?php include("./../templates/footer/footer.html"); ?>
+
+    <script src="./../templates/post/post.js"></script>
 </body>
 
 </html>
